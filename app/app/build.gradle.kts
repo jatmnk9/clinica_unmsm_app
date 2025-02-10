143668a081c9db66231eb5ea66c857645cb6790e
Mariano Villanueva <mariano.villanueva@unmsm.edu.pe>
2025-02-09 23:57:24 -0500

    Firebase configuration

--- app/app/build.gradle.kts
+++ app/app/build.gradle.kts
@@ -1,10 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp")
-    id("org.jetbrains.kotlin.kapt")
-    // agregar parcelize
-    id("kotlin-parcelize")
+    id("kotlin-parcelize") // Se mantiene si es necesario
+    id("com.google.gms.google-services") // Necesario para Firebase
 }
 
 android {
@@ -17,7 +15,6 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -30,6 +27,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -37,18 +35,9 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-
-    // Configuración de kapt para Room
-    kapt {
-        arguments {
-            // Define la ubicación de los esquemas para Room
-            arg("room.schemaLocation", "$projectDir/schemas")
-        }
-    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -57,11 +46,10 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.room.runtime)
-    implementation(libs.room.ktx)
-    kapt("androidx.room:room-compiler:2.6.1")
-    implementation("androidx.recyclerview:recyclerview:1.4.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore") // Agregar Firestore si lo usas
+    implementation("com.google.firebase:firebase-auth") // Para autenticación si lo usas
 }
